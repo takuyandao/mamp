263365cbee90c1ea3533d4fd7e42a4927beae9ca
takuyandao <takuyanmaru1212@gmail.com>
2022-03-08 10:33:58 +0900

    init

---
+++ src/php/controllers/home.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace controller\home;
+
+function get() {
+    
+    require_once SOURCE_BASE . 'views/home.php';
+
+}
---
+++ src/php/views/home.php
@@ -0,0 +1,6 @@
+    <h1>Homeページ</h1>
+    <!-- <img src="<?php echo BASE_IMAGE_PATH ?>logo.svg"> -->
+
+<form action="<?php echo BASE_CONTEXT_PATH ?>login" method="POST">
+    <input type="submit">
+</form>
